1dcb455fa60a047f331ad657a4f72487a21a1bb9
Dormouse Young <dormouse.young@gmail.com>
2017-10-26 22:30:03 +0800

    change theme to classic for static side bar

--- Makefile
+++ Makefile
@@ -7,12 +7,21 @@ SPHINXBUILD   = sphinx-build
 SPHINXPROJ    = DormouseBlog
 SOURCEDIR     = source
 BUILDDIR      = build
+GITHUB_DIR=~/project/dormouse.github.io
 
 # Put it first so that "make" without argument is like "make help".
 help:
 	@$(SPHINXBUILD) -M help "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
 
-.PHONY: help Makefile
+git:
+	@echo
+	@echo "Rsync starting..."
+	rsync -a $(BUILDDIR)/html/ ~/project/dormouse.github.io --exclude ".*"
+	cd $(GITHUB_DIR) && git pull && git add -A && git commit -m 'update' && git push origin $(GITHUB_PAGES_BRANCH)
+	@echo
+	@echo "Rsync finished."
+
+.PHONY: help git Makefile
 
 # Catch-all target: route all unknown targets to Sphinx using the new
 # "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
--- source/conf.py
+++ source/conf.py
@@ -86,13 +86,17 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = "sphinxdoc"
+# html_theme = "sphinxdoc"
+html_theme = "classic"
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
-
+html_theme_options = {
+    "stickysidebar": "true",
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
@@ -103,13 +107,17 @@ html_last_updated_fmt = '%b %d, %Y'
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
+"""
 html_sidebars = {
     '**': [
+        'about.html',
+        'navigation.html',
         'relations.html',  # needs 'show_related': True theme option to display
         'searchbox.html',
+        'donate.html',
     ]
 }
-
+"""
 
 # -- Options for HTMLHelp output ------------------------------------------
 
